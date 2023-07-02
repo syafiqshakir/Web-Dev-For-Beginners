195e3f96793864e7093a34b803482f39e25533eb
Syafiq <syafiq.shakir@gmail.com>
2023-07-02 22:45:01 +0800

    added new files

---
+++ 3-terrarium/terrarium/script.js
@@ -0,0 +1,17 @@
+function dragElement(terrariumElement) {
+    //set 4 positions for positioning on the screen
+    let pos1 = 0,
+        pos2 = 0,
+        pos3 = 0,
+        pos4 = 0;
+    terrariumElement.onpointerdown = pointerDrag;
+
+    function pointerDrag(e) {
+        e.preventDefault();
+        console.log(e);
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onpointermove = elementDrag;
+        document.onpointerup = stopElementDrag;
+    }
+}
